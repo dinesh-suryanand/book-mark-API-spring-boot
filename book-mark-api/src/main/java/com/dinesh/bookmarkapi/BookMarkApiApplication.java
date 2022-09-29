d25e65b0009f56b4532aa2138970064ed0d053f2
dinesh <dineshsuryanand.edu@gmail.com>
2022-09-29 11:08:06 +0530

    created basic project with dependencies

---
+++ book-mark-api/src/main/java/com/dinesh/bookmarkapi/BookMarkApiApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.bookmarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarkApiApplication.class, args);
+	}
+
+}
